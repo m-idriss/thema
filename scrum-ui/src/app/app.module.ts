663014188e6008e793a76e7f43126b5357b3121d
idriss <gitDAniel&22>
2021-11-28 18:26:17 +0100

    first commit

---
+++ scrum-ui/src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+import { HomeComponent } from './home/home.component';
+import { ScrumComponent } from './scrum/scrum.component';
+import { TaskDialogComponent } from './task-dialog/task-dialog.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ScrumDialogComponent } from './scrum-dialog/scrum-dialog.component';
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ScrumComponent,
+    TaskDialogComponent,
+    ScrumDialogComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatListModule,
+    DragDropModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatInputModule,
+    MatSelectModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  entryComponents: [TaskDialogComponent, ScrumDialogComponent]
+})
+export class AppModule { }
